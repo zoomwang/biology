6fc8b1ad4e396dfe6fe5314e6993e0f51d2800e3
wangrongrong <wangrongrong@bigo.sg>
2024-03-07 15:37:17 +0800

    s

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import './assets/main.scss';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/reset.css';
+// import Antd from 'ant-design-vue';
+// import 'ant-design-vue/dist/reset.css';
 import axios from 'axios';
-import { isLogged } from "@/services/user";
+// import { isLogged } from "@/services/user";
 // import localStorage from "./hooks/localStorage.js";
 // Vue.use(Antd)
 import dayjs from 'dayjs';
@@ -17,6 +17,6 @@ const app = createApp(App);
 app.config.globalProperties.$http = axios;
 
 app.use(router);
-app.use(Antd).mount('#app');
+// app.use(Antd).mount('#app');
 
-// app.mount('#app')
+app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
@@ -10,14 +12,21 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-viteCompression({
-  verbose: true, // 默认即可
-  disable: false, // 开启压缩(不禁用)，默认即可
-  deleteOriginFile: false, // 删除源文件
-  threshold: 5120, // 压缩前最小文件大小
-  algorithm: 'gzip', // 压缩算法
-  ext: '.gz' // 文件类型
-})
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: 'less', // 一定要开启这个配置项
+        }),
+      ],
+    }),
+    viteCompression({
+      verbose: true, // 默认即可
+      disable: false, // 开启压缩(不禁用)，默认即可
+      deleteOriginFile: false, // 删除源文件
+      threshold: 5120, // 压缩前最小文件大小
+      algorithm: 'gzip', // 压缩算法
+      ext: '.gz' // 文件类型
+    })
   ],
   envDir: "./src",
   resolve: {
