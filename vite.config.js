ba5d7f9a2926d29d6510744143fb3ba9f8106240
wangrongrong <wangrongrong@bigo.sg>
2024-03-07 14:22:14 +0800

    s

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+viteCompression({
+  verbose: true, // 默认即可
+  disable: false, // 开启压缩(不禁用)，默认即可
+  deleteOriginFile: false, // 删除源文件
+  threshold: 5120, // 压缩前最小文件大小
+  algorithm: 'gzip', // 压缩算法
+  ext: '.gz' // 文件类型
+})
   ],
   envDir: "./src",
   resolve: {
