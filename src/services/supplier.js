667b21e7e06a301c60fc54bc6e8dc31ef14ccb13
wangrongrong <wangrongrong@bigo.sg>
2024-05-05 12:04:35 +0800

    save

--- src/services/supplier.js
+++ src/services/supplier.js
@@ -10,12 +10,12 @@ export const supplierList = (param) => {
   return api.post(`/sys/supplier/list`, param);
 }
 
-/** 供应商列表 */
+/** 供应商界面列表 */
 export const supplierItemList = (param) => {
   return api.post(`/sys/supplier/item/list`, param);
 }
 
-/** 供应商列表 */
+/** 供应商项目列表 */
 export const supplierItemDetailList = (param) => {
   return api.post(`/sys/supplier/item-detail/list`, param);
 }
--- src/views/user/Manage/supplier/SupplierList.vue
+++ src/views/user/Manage/supplier/SupplierList.vue
@@ -9,12 +9,8 @@ import {
   watch,
 } from "vue";
 import {
-  getOrderLists,
-  cancelOrder,
-  getOrderInfo,
-  getReturnPriceDiff,
-  getPayPriceDiff,
-} from "../../../../services/process";
+  supplierItemList
+} from "../../../../services/supplier";
 import { notification } from "ant-design-vue";
 import {formatTime} from "@/utils/index";
 import DownLoad from "@/components/DownLoad.vue";
@@ -42,62 +38,46 @@ const param = reactive({
   pageSize: 999,
   curPage: 1,
   status: "0",
-  startTime: "",
-  endTime: "",
+  itemname: ""
 });
-
-const orderColums = [
-  {
-    title: "样品数量",
-    dataIndex: "count",
-    key: "count",
-  },
-  {
-    title: "实验目的",
-    dataIndex: "goal",
-    key: "age",
-  },
-  {
-    title: "样品编号",
-    dataIndex: "numberList",
-    key: "address",
-    slots: {
-      customRender: "numberList",
-    },
-  },
-];
 
 const columns = [
   {
     title: "项目名称",
     dataIndex: "itemname",
+    key: "itemname",
   },
   {
-    title: "下单时间",
-    className: "column-money",
-    dataIndex: "createTime",
-  },
-  {
-    title: "订单号",
-    dataIndex: "orderId",
-  },
-  {
-    title: "实付款",
-    dataIndex: "costInfo",
-    slots: {
-      customRender: "costInfo",
-    },
-  },
-  {
-    title: "订单状态",
+    title: "状态",
     dataIndex: "status",
+    key: "status",
     slots: {
       customRender: "status",
     },
   },
   {
-    title: "退补差价",
-    dataIndex: "priceDifference",
+    title: "创建时间",
+    dataIndex: "createime",
+    key: "createime",
+    slots: {
+      customRender: "createime",
+    },
+  },
+  {
+    title: "供应商数量",
+    dataIndex: "supplierNumber",
+    key: "supplierNumber",
+    slots: {
+      customRender: "supplierNumber",
+    },
+  },
+  {
+    title: "订单数量",
+    dataIndex: "orderNumber",
+    key: "orderNumber",
+    slots: {
+      customRender: "orderNumber",
+    },
   },
   {
     title: "操作",
@@ -128,47 +108,6 @@ const formState = reactive({
 
 const dataSource = ref([]);
 
-const getPayPriceDiffs = async (params) => {
-  try {
-    const res = await getPayPriceDiff({
-      orderId: params.orderId,
-      payPlatform: params.payPlatform
-    });
-    if (res?.code == 0) {
-      if (params.payPlatform > 0) {
-        diffPayData.value = {
-          codeUrl: res.data,
-          payType: params.payPlatform,
-          cost: params.priceDifference,
-          orderId: params.orderId
-        }
-        diffVisible.value = true;
-      } 
-      if (params.payPlatform  == 0) {
-        notification.success({
-          description: "补差价成功",
-        });
-        getOrderList();
-      }
-    } else {}
-  } catch (err) {}
-};
-
-const getReturnPriceDiffs = async (params) => {
-  try {
-    const res = await getReturnPriceDiff({
-      orderId: params.orderId,
-      payPlatform: params.payPlatform
-    });
-    if (res?.code == 0) {
-      notification.success({
-        description: "退差价成功",
-      });
-      getOrderList();
-    }
-  } catch (err) {}
-};
-
 const getOrderInfos = async (params, type) => {
   try {
     const res = await getOrderInfo(params);
@@ -179,26 +118,6 @@ const getOrderInfos = async (params, type) => {
       } else {
         drawerVisible.value = true;
       }
-    }
-  } catch (err) {}
-};
-
-const successCall = () => {
-  getOrderList();
-  drawerVisible.value = false;
-  diffVisible.value = false;
-}
-
-const cancelOrders = async (orderId) => {
-  try {
-    const res = await cancelOrder({
-      orderId,
-    });
-    if (res?.code == 0) {
-      getOrderList();
-      notification.success({
-        description: "取消订单成功",
-      });
     }
   } catch (err) {}
 };
@@ -218,28 +137,23 @@ onMounted(() => {
   getOrderList();
 });
 
-const menus = ["待报价", "可支付", "待实验", "实验中", "已完成","欠款中","已开票","已还款", "已取消"];
+const menus = ["已上架", "已下架"];
 </script>
 
 <template>
   <!-- 用户注册资料 -->
   <main>
     <a-form style="margin: 10px 10px 20px 0" :model="formState" layout="inline" :label-col="labelCol" :wrapper-col="wrapperCol">
-      <a-form-item label="" :wrapperCol="{
+      <a-form-item label="项目名称" :wrapperCol="{
         span: 7
       }">
         <a-input v-model:value="param.name" placeholder="测试项目" style="width:140px" />
-      </a-form-item>
-      <a-form-item label="" :wrapperCol="{
-        span: 5
-      }">
-        <a-select v-model:value="param.status" placeholder="全部分组" style="width:120px" />
       </a-form-item>
       <a-form-item label="订单状态" :wrapperCol="{
         span: 7
       }">
         <a-select v-model:value="param.status" style="width: 100px">
-          <a-select-option value="0">全部订单</a-select-option>
+          <a-select-option value="-1">全部订单</a-select-option>
           <a-select-option v-for="(item, index) in menus" :key="item" :value="++index">{{ item }}</a-select-option>
         </a-select>
       </a-form-item>
@@ -260,19 +174,7 @@ const menus = ["待报价", "可支付", "待实验", "实验中", "已完成","
           {{ menus[--text] }}
         </span>
       </template>
-      <template #costInfo="{ text }">
-        <span>
-          {{ text["支付金额"] }}
-        </span>
-      </template>
-      <template #ticketStatus="{ text }">
-        <span>
-          {{ ["无需开票", "无", "待申请"][text] }}
-        </span>
-      </template>
       <template #action="{ record }">
-        
-        <br v-if="record.status <=2 || record.priceDifferenceStatus == 1" />
         <a-button type="text" @click="showModal(record.orderId)"
           >更多详情</a-button
         >
