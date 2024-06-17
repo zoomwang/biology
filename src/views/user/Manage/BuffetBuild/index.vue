e691dd38b68c74517900a034cec8a2cf35b34990
yolo <weixu416@gmail.com>
2024-06-17 23:29:10 +0800

    feat: 自助下单 - 基础结构

--- src/views/user/Manage/BuffetBuild/index.vue
+++ src/views/user/Manage/BuffetBuild/index.vue
@@ -1,7 +1,22 @@
 <template>
-    <div>{{msg}}</div>
+  <a-tabs v-model:activeKey="activeKey" type="card">
+    <a-tab-pane v-for="item in tabs" :key="item.value" :tab="item.label">
+      <component :is="item.comp" />
+    </a-tab-pane>
+  </a-tabs>
 </template>
 <script setup>
-import {ref} from 'vue'
-const msg = ref('自助下单')
+import { ref } from "vue";
+import BasicInfo from "./Modules/BasicInfo.vue";
+import Form1 from "./Modules/Form1.vue";
+import Form2 from "./Modules/Form2.vue";
+import DetailSetting from "./Modules/DetailSetting.vue";
+const tabs = [
+  { label: "基础信息", value: "BasicInfo", comp: BasicInfo },
+  { label: "下单页面一", value: "form1", comp: Form1 },
+  { label: "下单页面二", value: "form2", comp: Form2 },
+  { label: "详细设置", value: "detailSetting", comp: DetailSetting },
+];
+const activeKey = ref(tabs[0].value);
 </script>
+
