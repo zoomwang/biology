6d58905d8d9c4d126ce8175367849a02ad43509c
yolo <weixu416@gmail.com>
2024-06-28 00:00:10 +0800

    feat: 自助下单 - 基础信息 - 保存

--- src/views/user/Manage/BuffetBuild/Modules/BasicInfo.vue
+++ src/views/user/Manage/BuffetBuild/Modules/BasicInfo.vue
@@ -1,103 +1,149 @@
 <template>
-  <a-form :model="formState" :label-col="labelCol" :wrapper-col="wrapperCol">
-    <div style="word-wrap: break-word;white-space:pre-wrap; ">
-    {{ formState }}
-  </div>
-    <a-form-item label="设备信息">
-      <a-input v-model:value="formState.name" />
+  <a-form ref="formRef" :model="formState" :label-col="labelCol" :wrapper-col="wrapperCol" :rules="rules">
+    <a-form-item name="deviceName" label="设备名称">
+      <a-input placeholder="请输入" v-model:value="formState.deviceName" />
     </a-form-item>
-    <a-form-item label="设备别名">
-      <a-input v-model:value="formState.alias" />
+    <a-form-item name="deviceAliases" label="设备别名">
+      <a-input placeholder="请输入" v-model:value="formState.deviceAliases" />
     </a-form-item>
-    <a-form-item label="设备型号">
-      <a-input v-model:value="formState.types" />
+    <a-form-item name="deviceModel" label="设备型号">
+      <a-input placeholder="请输入" v-model:value="formState.deviceModel" />
     </a-form-item>
-    <a-form-item label="品牌">
-      <a-input v-model:value="formState.brand" />
+    <a-form-item name="brand" label="品牌">
+      <a-input placeholder="请输入" v-model:value="formState.brand" />
     </a-form-item>
     <a-form-item label="类别">
-      <a-checkbox-group v-model:value="formState.categorys">
-        <div
-          v-for="(subCategorys, category) in DETECT_CATEGORY_TYPES"
-          :key="category"
-        >
+      <a-checkbox-group v-model:value="formState.categorys" v-if="0">
+        <div v-for="(subCategorys, category) in DETECT_CATEGORY_TYPES" :key="category">
           <a-checkbox :value="category"> {{ category }}: </a-checkbox>
           <a-checkbox-group v-model:value="formState.subCategorys[category]">
-            <a-checkbox
-              v-for="subCategory in subCategorys"
-              :key="subCategory"
-              :value="subCategory"
-            >
+            <a-checkbox v-for="subCategory in subCategorys" :key="subCategory" :value="subCategory">
               {{ subCategory }}
             </a-checkbox>
           </a-checkbox-group>
         </div>
       </a-checkbox-group>
+      <a-tree-select v-model:value="formState.projectType" show-search style="width: 100%"
+        :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }" placeholder="请选择" allow-clear tree-default-expand-all
+        :tree-data="projectTypeTree" tree-node-filter-prop="title" :field-names="{
+          children: 'children',
+          value: 'value',
+          label: 'title',
+        }">
+      </a-tree-select>
     </a-form-item>
-    <a-form-item label="列表图">
-      <Upload v-model:value="formState.listPic" :max-count="3"></Upload>
+    <a-form-item name="pictureUrl" label="列表图">
+      <Upload v-model:value="formState.pictureUrl" :max-count="1"></Upload>
     </a-form-item>
-    <a-form-item label="设备总负责人">
-      <a-select v-model:value="formState.resource" :options="userList">
-      </a-select>
+    <a-form-item name="deviceSuperintendent" label="设备总负责人">
+      <a-input placeholder="请输入" v-model:value="formState.deviceSuperintendent" />
     </a-form-item>
-    <a-form-item label="下单通知邮箱">
-      <a-input v-model:value="formState.email" />
+    <a-form-item name="notificationEmail" label="下单通知邮箱">
+      <a-input placeholder="请输入" v-model:value="formState.notificationEmail" />
     </a-form-item>
-    <a-form-item label="注意事项">
-      <Editor style="height: 500px;" v-model:value="formState.notify" placeholder="请输入注意事项" />
-    </a-form-item>
-    <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
-      <a-button type="primary" @click="onSubmit">Create</a-button>
-      <a-button style="margin-left: 10px">Cancel</a-button>
+    <a-form-item name="attention" label="注意事项">
+      <Editor style="height: 500px;" v-model:value="formState.attention" placeholder="请输入注意事项" />
     </a-form-item>
   </a-form>
 </template>
-<script  setup>
-import { reactive, toRaw, ref } from "vue";
+<script setup>
+import { reactive, toRaw, ref, onMounted, watch } from "vue";
 import { DETECT_CATEGORY_TYPES } from "@/utils/const";
+// import { supplierList } from '@/services/supplier'
 import Editor from "@/components/Editor.vue";
 import Upload from "@/components/Upload.vue";
-const userList = reactive([
-  { label: "张总", value: "张总" },
-  { label: "张总1", value: "张总1" },
-]);
-const formState = reactive({
-  name: "",
-  delivery: false,
-  categorys: [],
-  subCategorys: {},
-  resource: "",
-  desc: "",
-  email: "",
-  notify: "",
-  listPic: ['https://etest-oss.oss-cn-beijing.aliyuncs.com/20240623/xps656_6531_52f68dec4e4202247f184d71bc40b9c4.png'],
-});
-const onSubmit = () => {
-  console.log("submit!", toRaw(formState));
-};
+
+const props = defineProps({ model: Object })
+
+
 const labelCol = { style: { width: "150px" } };
 const wrapperCol = { span: 14 };
+// const userList = ref([]);
+const projectTypeTree = ref([])
 
-const fileList = ref([
-  {
-    uid: '-1',
-    name: 'image.png',
-    status: 'done',
-    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-  },
-  {
-    uid: '-xxx',
-    percent: 50,
-    name: 'image.png',
-    status: 'uploading',
-    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-  },
-  {
-    uid: '-5',
-    name: 'image.png',
-    status: 'error',
-  },
-]);
+const formRef = ref();
+const formState = reactive({
+  deviceName: undefined,
+  deviceAliases: undefined,
+  types: undefined,
+  brand: undefined,
+  pictureUrl: undefined,
+  deviceSuperintendent: undefined,
+  notificationEmail: undefined,
+  attention: undefined,
+  // categorys: [],
+  // subCategorys: {}
+});
+const rules = {
+  deviceName: [{ required: true, message: '必填项', trigger: 'change' }],
+}
+
+watch(() => props.model, (model) => {
+  setFormValue(model)
+}, { immediate: true })
+
+
+function setFormValue(model) {
+  Object.assign(formState, model)
+
+}
+function getFormValue() {
+  return { baseInfo: toRaw(formState) }
+}
+
+async function validate() {
+  await formRef.value.validate()
+  return getFormValue()
+}
+
+defineExpose({
+  getFormValue,
+  validate,
+})
+// const getUserList = async () => {
+//   const params = {
+//     curPage: 1,
+//     pageSize: 999,
+//   }
+//   const {data} = await supplierList(params)
+//   userList.value = (data.list||[]).map(item => {
+//     return {
+//       label: item.supplierName,
+//       value: item.supplierId,
+//     }
+//   })
+// }
+
+const getProjectTypeTree = async () => {
+  projectTypeTree.value = [
+    {
+      title: '高端测试',
+      value: 2,
+      selectable: false,
+      children: [
+        {
+          title: '同步辐射类',
+          value: 1501,
+        },
+        {
+          title: '高端电镜类',
+          value: 1502,
+        },
+        {
+          title: '高端原位类',
+          value: 1503,
+        },
+        {
+          title: '高端质谱',
+          value: 1504,
+        },
+      ],
+    },
+  ]
+}
+
+onMounted(() => {
+  // getUserList()
+  getProjectTypeTree()
+})
 </script>
-
--- src/views/user/Manage/BuffetBuild/index.vue
+++ src/views/user/Manage/BuffetBuild/index.vue
@@ -1,16 +1,25 @@
 <template>
-  <a-tabs v-model:activeKey="activeKey" type="card">
-    <a-tab-pane v-for="item in tabs" :key="item.value" :tab="item.label">
-      <component :is="item.comp" />
-    </a-tab-pane>
-  </a-tabs>
+  <a-form>
+    <a-tabs v-model:activeKey="activeKey" type="card">
+      <a-tab-pane v-for="item in tabs" :key="item.value" :tab="item.label">
+        <component ref="formRef" :is="item.comp" :model="BuffetBuildData.baseInfo" />
+      </a-tab-pane>
+    </a-tabs>
+    <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
+      <a-button type="primary" :loading="submitting" @click="onSubmit">保存</a-button>
+    </a-form-item>
+  </a-form>
 </template>
 <script setup>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 import BasicInfo from "./Modules/BasicInfo.vue";
 import Form1 from "./Modules/Form1.vue";
 import Form2 from "./Modules/Form2.vue";
 import DetailSetting from "./Modules/DetailSetting.vue";
+import { updateConfig } from '@/services/buffet-build'
+
+const formRef = ref()
+const submitting = ref(false)
 const tabs = [
   { label: "基础信息", value: "BasicInfo", comp: BasicInfo },
   { label: "下单页面一", value: "form1", comp: Form1 },
@@ -18,5 +27,20 @@ const tabs = [
   { label: "详细设置", value: "detailSetting", comp: DetailSetting },
 ];
 const activeKey = ref(tabs[0].value);
-</script>
+const BuffetBuildData = reactive({
+  baseInfo: { "deviceName": "s", "deviceAliases": "r", "brand": "r", "pictureUrl": "https://etest-oss.oss-cn-beijing.aliyuncs.com/20240627/微信截图_20240627230411_83686_fa5f3db097724696b4fc8ac4d9b68b9b.png", "deviceSuperintendent": "2", "notificationEmail": "2", "attention": "<p>222</p>", "deviceModel": "r", "projectType": 1501, "orderTypeId": 1501 }
+})
 
+const onSubmit = async () => {
+  console.log(formRef.value[0])
+  try {
+    submitting.value = true
+    const form = await formRef.value[0].validate()
+    await updateConfig({ ...form, orderTypeId: 1501 })
+  } catch (error) {
+
+  } finally {
+    submitting.value = false
+  }
+}
+</script>
