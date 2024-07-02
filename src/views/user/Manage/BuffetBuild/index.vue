fe00f64df1e651384157db8fe9705eda2a8e7fa7
yolo <weixu416@gmail.com>
2024-07-02 23:26:06 +0800

    feat: 自助下单 - 上下架, 删除

--- src/views/user/Manage/BuffetBuild/index.vue
+++ src/views/user/Manage/BuffetBuild/index.vue
@@ -3,19 +3,24 @@
     <div style="padding: 16px">
       <a-button @click="handleCreate">新建</a-button>
     </div>
-    <a-table
-      :columns="columns"
-      :data-source="dataSource"
-      :pagination="pagination"
-      :loading="loading"
-      @change="handleTableChange"
-    >
+    <a-table :columns="columns" :data-source="dataSource" :pagination="pagination" :loading="loading"
+      @change="handleTableChange">
       <template #bodyCell="{ column, record }">
         <template v-if="column.key === 'action'">
-          <a
-            :href="`/user/buffet-build/create-and-update?typeId=${record.orderTypeId}`"
-            >编辑</a
-          >
+          <a-space :gap="4">
+            <a-button type="link" :href="`/user/buffet-build/create-and-update?typeId=${record.orderTypeId}`">
+              编辑
+            </a-button>
+            <a-popconfirm v-if="!record.isListing" title="确定要上架吗?" @confirm="handleshelveOn(record)">
+              <a-button type="link">上架</a-button>
+            </a-popconfirm>
+            <a-popconfirm v-if="record.isListing" title="确定要下架吗?" @confirm="handleshelveOff(record)">
+              <a-button type="link">下架</a-button>
+            </a-popconfirm>
+            <a-popconfirm title="确定要删除吗?" @confirm="handleDelete(record)">
+              <a-button type="link" danger >删除</a-button>
+            </a-popconfirm>
+          </a-space>
         </template>
       </template>
     </a-table>
@@ -25,7 +30,14 @@
 import { ref, computed } from "vue";
 import { useRouter } from "vue-router";
 import { usePagination } from "vue-request";
-import { fetchConfigList } from "@/services/buffet-build";
+import { message } from "ant-design-vue";
+
+
+import {
+  fetchConfigList, deleteConfig,
+  shelveOnConfig,
+  shelveOffConfig,
+} from "@/services/buffet-build";
 const router = useRouter();
 
 const columns = [
@@ -45,10 +57,14 @@ const columns = [
     title: "品牌",
     dataIndex: "brand",
   },
-  // {
-  //   title: "类别",
-  //   dataIndex: "projectType",
-  // },
+  {
+    title: "类别",
+    dataIndex: "projectType",
+    customRender: ({ record }) => {
+      return `${record.projectTypeName}/${record.projectSubTypeName}`
+    }
+  },
+
   {
     title: "操作",
     key: "action",
@@ -59,17 +75,6 @@ const total = ref(0);
 const queryData = params => {
   return fetchConfigList(params).then(res => {
     const list = res.data.list
-      .filter(
-        item =>
-          item.dynamicFormConfig?.orderTypeId &&
-          item.dynamicFormConfig?.baseInfo
-      )
-      .map(item => {
-        return {
-          orderTypeId: item.id,
-          ...item.dynamicFormConfig.baseInfo,
-        };
-      });
     console.log(list);
     total.value = list.length;
     return list;
@@ -82,16 +87,13 @@ const {
   loading,
   pageSize,
   current,
+  refresh,
 } = usePagination(queryData, {
   defaultParams: [
     {
       pageSize: 100,
     },
   ],
-  // formatResult: res => {
-  //   debugger
-  //   return res.data.list;
-  // },
   pagination: {
     currentKey: "curPage",
     pageSizeKey: "pageSize",
@@ -115,4 +117,28 @@ const handleTableChange = (pag, filters) => {
 const handleCreate = () => {
   router.push("/user/buffet-build/create-and-update");
 };
+
+const handleDelete = async (row) => {
+  console.log(row)
+  await deleteConfig({ orderTypeId: row.orderTypeId })
+  message.success('操作成功');
+  refresh()
+}
+
+const handleshelveOn = async (row) => {
+  console.log(row)
+  await shelveOnConfig({ orderTypeId: row.orderTypeId })
+  message.success('操作成功');
+  refresh()
+}
+
+const handleshelveOff = async (row) => {
+  console.log(row)
+  await shelveOffConfig({ orderTypeId: row.orderTypeId })
+  message.success('操作成功');
+  refresh()
+}
+
+
+
 </script>
