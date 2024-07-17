404805c597d82eadce4195ffaa6b9aee669de53a
yolo <weixu416@gmail.com>
2024-07-17 22:50:19 +0800

    feat: 调整请求超时时间

--- src/services/common.js
+++ src/services/common.js
@@ -1,5 +1,5 @@
 import api from './index'
 
 export const uploadFile = (param, config) => {
-  return api.post(`/sys/file/upload`, param, config);
+  return api.post(`/sys/file/upload`, param, {...config, timeout: 0});
 }
